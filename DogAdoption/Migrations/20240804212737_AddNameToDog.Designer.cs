12cedbadc4693a1755bd0ae4bc32e703b4c5eda7
andrew.seo <andrew.seo@vanderbilt.edu>
2024-08-04 16:31:50 -0500

    added name column to dogs database

--- DogAdoption/Migrations/20240804212737_AddNameToDog.Designer.cs
+++ DogAdoption/Migrations/20240804212737_AddNameToDog.Designer.cs
@@ -3,6 +3,7 @@ using DogAdoption.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DogAdoption.Migrations
 {
     [DbContext(typeof(DogAdoptionContext))]
-    partial class DogAdoptionContextModelSnapshot : ModelSnapshot
+    [Migration("20240804212737_AddNameToDog")]
+    partial class AddNameToDog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DogAdoption/Models/Dog.cs
+++ DogAdoption/Models/Dog.cs
@@ -3,6 +3,7 @@
     public class Dog
     {
         public int Id { get; set; }
+        public string Name { get; set; }    
         public string Breed { get; set; }
         public int age { get; set; }
         public string ImageUrl { get; set; }
